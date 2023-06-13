c5f4825fa959a9613150ca9c56387fd5f99a35f3
Matthias Varberg Ingesman <mvi@ingesman.dk>
2023-06-13 14:16:26 +0200

    feat: bbin on path

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,7 @@ set -x PATH $PATH $ANDROID_HOME/platform-tools
 set -x PATH $PATH $ANDROID_HOME/tools $ANDROID_HOME/tools/bin $ANDROID_HOME/emulator
 set -x PATH $PATH $HOME/Programmer/Tmuxifier/bin $HOME/Programmer/Leiningen $HOME/Programmer/Boot $HOME/Programmer/git-madge
 set -x PATH $PATH $HOME/.local/bin
+set -x PATH $PATH $HOME/.babashka/bbin/bin
 set -x GITHUB_ACCESS_TOKEN 1172ec0c1a233e1e75e68339943c0dfa5fb6b4c1
 set -x MYVIMRC $HOME/.config/nvim/init.vim
 
