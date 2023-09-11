d322cbe1c5485421c013dee1395084e0d0f725f7
Matthias Varberg Ingesman <mvi@ingesman.dk>
2023-09-11 09:42:49 +0200

    feat: add structurizr-mode

--- emacs/init.el
+++ emacs/init.el
@@ -111,6 +111,10 @@
 (global-set-key (kbd "M-i") 'imenu)
 
 
+(add-to-list 'load-path "~/.config/emacs/lisp")
+(require 'structurizr-mode)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
