058fdb1f9ca57cd8eae3d7bbb9484d6f6853966f
Matthias Varberg Ingesman <mvi@ingesman.dk>
2023-12-26 21:58:12 +0100

    feat(emacs): add gradle (Kotlin) project to projectile

--- emacs/lisp/languages/kotlin-config.el
+++ emacs/lisp/languages/kotlin-config.el
@@ -1,3 +1,11 @@
 ;;- Kotlin
 
 (use-package kotlin-mode)
+
+(when (package-installed-p 'projectile)
+  (require 'projectile)
+  (projectile-register-project-type 'gradle-kts '("build.gradle.kts")
+                                    :project-file "build.gradle.kts"
+                                    :compile "./gradlew build"
+                                    :test "./gradlew test"
+                                    :run "./gradlew bootRun"))
