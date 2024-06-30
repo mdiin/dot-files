754918fff1ad53334f411c1dd56b68d02bd2157a
Matthias Varberg Ingesman <mvi@ingesman.dk>
2024-06-30 22:25:59 +0200

    chore(emacs): remove forge package
    I wasn't using it...

--- emacs/lisp/other/git-config.el
+++ emacs/lisp/other/git-config.el
@@ -1,10 +1,4 @@
 ;;; Git
 
-(use-package magit)
-
-(add-to-list 'package-pinned-packages '(magit . "melpa") t)
-
-(use-package forge
-  :after magit)
-
-(add-to-list 'package-pinned-packages '(forge . "melpa") t)
+(use-package magit
+  :pin "melpa")
