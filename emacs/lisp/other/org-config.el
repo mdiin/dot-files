237a88d04a74edeee4adfb191505a949ad5b82f3
Matthias Varberg Ingesman <mvi@ingesman.dk>
2024-08-26 16:33:31 +0200

    chore(emacs): move org-mode specifics to org-config.el
    The rest stays in org-present-config.

--- emacs/lisp/other/org-config.el
+++ emacs/lisp/other/org-config.el
@@ -1,6 +1,51 @@
 ;;; Org
 
-(unless (package-installed-p 'org)
-  (package-install 'org))
+(use-package org
+  :init
+  (add-hook 'org-mode-hook (lambda () (variable-pitch-mode 1)))
 
-(require 'org)
+  :config
+  (set-face-attribute 'default nil :font "Fantasque Sans Mono" :weight 'light :height 120)
+  (set-face-attribute 'fixed-pitch nil :font "Fantasque Sans Mono" :weight 'light :height 120)
+  ;(set-face-attribute 'variable-pitch nil :font "Noto Sans" :weight 'light :height 1.3)
+  (set-face-attribute 'variable-pitch nil :font "Poppins" :weight 'light :height 1.3)
+  (setq org-hide-emphasis-markers t)
+  (dolist (face '((org-level-1 . (1.3 . "white"))
+                  (org-level-2 . (1.1 . "light grey"))
+                  (org-level-3 . (1.05 . nil))
+                  (org-level-4 . (1.0 . nil))
+                  (org-level-5 . (1.1 . nil))
+                  (org-level-6 . (1.1 . nil))
+                  (org-level-7 . (1.1 . nil))
+                  (org-level-8 . (1.1 . nil))))
+    (set-face-attribute (car face) nil :font "Noto Sans" :weight 'medium :height (cadr face) :foreground (cddr face)))
+
+  ;; Make the document title a bit bigger
+  (set-face-attribute 'org-document-title nil :font "Noto Sans" :weight 'bold :height 1.3)
+
+  ;; Make sure certain org faces use the fixed-pitch face when variable-pitch-mode is on
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+  )
+
+(require 'org-faces)
+
+(use-package visual-fill-column
+  :init
+  (add-hook 'org-mode-hook 'visual-line-fill-column-mode)
+
+  :config
+  (setq visual-fill-column-width 110
+        visual-fill-column-center-text 't)
+  )
+
+(use-package org-superstar
+  :pin "nongnu"
+  :init
+  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
--- emacs/lisp/other/org-present-config.el
+++ emacs/lisp/other/org-present-config.el
@@ -1,13 +1,9 @@
-(use-package visual-fill-column
-  :config
-  (setq visual-fill-column-width 110
-        visual-fill-column-center-text 't))
+(defconst my/stored-mode-line-format mode-line-format)
 
 (defun my/org-present-start ()
   (interactive)
-  (visual-line-fill-column-mode 1)
-  (menu-bar-mode 0)
 
+  (menu-bar-mode 0)
 
   (setq-local face-remapping-alist '((default (:height 1.5) variable-pitch)
                                      (header-line (:height 4.0) variable-pitch)
@@ -18,54 +14,38 @@
                                      (org-block-begin-line (:height 0.7) org-block)))
 
   (setq header-line-format " ")
+
+  (setq mode-line-format nil)
+
+  (org-display-inline-images)
   )
 
 (defun my/org-present-end ()
   (interactive)
+  (org-remove-inline-images)
   (menu-bar-mode 1)
-  (visual-line-fill-column-mode 0)
 
   (setq-local face-remapping-alist nil)
 
   (setq header-line-format nil)
+
+  (setq mode-line-format my/stored-mode-line-format)
   )
 
-(set-face-attribute 'default nil :font "Fantasque Sans Mono" :weight 'light :height 120)
-(set-face-attribute 'fixed-pitch nil :font "Fantasque Sans Mono" :weight 'light :height 120)
-(set-face-attribute 'variable-pitch nil :font "Noto Sans" :weight 'light :height 1.3)
+(defun my/org-present-prepare-slide (buffer-name heading)
+  ;; Show only top-level headlines
+  (org-overview)
 
-;; Hide emphasis markers on formatted text
-(setq org-hide-emphasis-markers t)
+  ;; Unfold the current entry
+  (org-show-entry)
 
-;; Resize Org headings
-(dolist (face '((org-level-1 . 1.2)
-                (org-level-2 . 1.1)
-                (org-level-3 . 1.05)
-                (org-level-4 . 1.0)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1)))
-  (set-face-attribute (car face) nil :font "Noto Sans" :weight 'medium :height (cdr face)))
-
-;; Make the document title a bit bigger
-(set-face-attribute 'org-document-title nil :font "Noto Sans" :weight 'bold :height 1.3)
-
-;; Make sure certain org faces use the fixed-pitch face when variable-pitch-mode is on
-(set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-table nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-formula nil :inherit 'fixed-pitch)
-(set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-(set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-(set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
-
-(require 'org-faces)
+  ;; Show only direct subheadings of the slide but don't expand them
+  (org-show-children)
+  )
 
 (use-package org-present
-  :after visual-fill-column
   :init
   (add-hook 'org-present-mode-hook 'my/org-present-start)
   (add-hook 'org-present-mode-quit-hook 'my/org-present-end)
+  (add-hook 'org-present-after-navigate-functions 'my/org-present-prepare-slide)
   )
