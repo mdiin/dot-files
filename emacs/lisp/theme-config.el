014ecf6cd55ab07c58eb42842cfc5e9b6996a9cf
Matthias Varberg Ingesman <mvi@trifork.com>
2025-03-07 16:32:50 +0100

    feat(emacs): update colorscheme to something with more contrast

--- emacs/lisp/theme-config.el
+++ emacs/lisp/theme-config.el
@@ -4,4 +4,10 @@
   (package-install 'solarized-theme))
 
 (require 'solarized-theme)
-(load-theme 'solarized-zenburn t)
+;; (load-theme 'solarized-zenburn t)
+
+(use-package modus-themes
+  :config
+  (setq modus-themes-italic-constructs t
+        modus-themes-bold-constructs nil)
+  (load-theme 'modus-vivendi-tinted))
