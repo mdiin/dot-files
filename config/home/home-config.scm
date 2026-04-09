2a052bf6894014e321d7868bb3c7d09092bd85c0
Matthias Varberg Ingesman <mvi@ingesman.dk>
2026-04-09 14:53:26 +0200

    feat(home): add host to ssh config

--- config/home/home-config.scm
+++ config/home/home-config.scm
@@ -13,7 +13,12 @@
 
  (services (list (service home-openssh-service-type
                           (home-openssh-configuration
-                           (add-keys-to-agent "yes")))
+                           (add-keys-to-agent "yes")
+                           (hosts (list
+                                   (openssh-host
+                                    (name "ssh.dev.azure.com")
+                                    (identity-file "/home/mvi/.ssh/id_rn_rsa")
+                                    (extra-content "  IdentitiesOnly yes\n  LogLevel FATAL"))))))
 
                  (service home-ssh-agent-service-type
                           (home-ssh-agent-configuration
