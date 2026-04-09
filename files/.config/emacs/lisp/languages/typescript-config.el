f4bc0eca222993d70753a6fe9658a8dfbce6945b
Matthias Varberg Ingesman <mvi@ingesman.dk>
2026-04-09 16:44:34 +0200

    chore(emacs): improve typescript config

--- files/.config/emacs/lisp/languages/typescript-config.el
+++ files/.config/emacs/lisp/languages/typescript-config.el
@@ -2,10 +2,15 @@
 
 (use-package prettier)
 
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-ts-mode))
-
 (defun config/typescript-setup ()
   (eglot-ensure)
   (config/maybe-treesit-install-language-grammar 'typescript))
 
-(add-hook 'typescript-ts-mode 'config/typescript-setup)
+(use-package typescript-ts-mode
+  :ensure nil
+  :mode "\\.ts\\'"
+  :hook ((typescript-ts-mode . config/typescript-setup)))
+
+(use-package tsx-ts-mode
+  :ensure nil
+  :mode "\\.tsx\\'")
